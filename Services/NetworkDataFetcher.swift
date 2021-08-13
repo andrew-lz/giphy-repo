7317415f025f79c1d828913f1cdf48acbeef76b0
andrew-lz <andrewlazerko@gmail.com>
2021-08-13 08:04:41 +0300

    Added .gitignore

--- Services/NetworkDataFetcher.swift
+++ Services/NetworkDataFetcher.swift
@@ -28,20 +28,19 @@ class NetworkDataFetcher {
     }
 }
 
-
 private class NetworkService {
-    func request(urlString: String, completion: @escaping (Result<Data, Error>) -> Void) {
-       guard let url = URL(string: urlString) else { return }
-       URLSession.shared.dataTask(with: url) { (data, _, error) in
-           DispatchQueue.main.async {
-               if let error = error {
-                   print("Some error")
-                   completion(.failure(error))
-                   return
-               }
-               guard let data = data else { return }
-               completion(.success(data))
-           }
-       }.resume()
-   }
+     func request(urlString: String, completion: @escaping (Result<Data, Error>) -> Void) {
+        guard let url = URL(string: urlString) else { return }
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            DispatchQueue.main.async {
+                if let error = error {
+                    print("Some error")
+                    completion(.failure(error))
+                    return
+                }
+                guard let data = data else { return }
+                completion(.success(data))
+            }
+        }.resume()
+    }
 }
