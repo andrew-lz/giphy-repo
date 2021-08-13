6412ec8c9f90a38af57fceb959a65e5bd2d27c3f
andrew-lz <andrewlazerko@gmail.com>
2021-08-13 14:00:37 +0300

    Fixed code style problem N2

---
+++ GiphyView/View/CustomCollectionViewCell.swift
@@ -0,0 +1,54 @@
+//
+//  CustomCollectionViewCell.swift
+//  GiphyView
+//
+//  Created by BMF on 1.08.21.
+//  Copyright © 2021 Zero. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    static var identifier: String = "CustomCollectionViewCell"
+    let loadingIndicator: UIActivityIndicatorView = {
+        let loadingIndicator = UIActivityIndicatorView()
+        loadingIndicator.color = .black
+        return loadingIndicator
+    }()
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadingIndicator.center = contentView.center
+        contentView.addSubview(loadingIndicator)
+        contentView.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.topAnchor
+                .constraint(equalTo: contentView.topAnchor),
+            imageView.leftAnchor
+                .constraint(equalTo: contentView.leftAnchor),
+            imageView.rightAnchor
+                .constraint(equalTo: contentView.rightAnchor),
+            imageView.bottomAnchor
+                .constraint(equalTo: contentView.bottomAnchor)
+        ])
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
+}
